0737b2667d4f00e43dcb371de5f0126e67376ba6
lemkay5 <104469576+lemkay5@users.noreply.github.com>
2022-04-27 10:27:44 -0400

    Add files via upload
    File with working title and author searches.

--- LibraryCatalog.py
+++ LibraryCatalog.py
@@ -9,9 +9,11 @@ catalogList = []
 
 def main():
     housekeeping()
-    print(catalogList)
-    
-    
+    choice = chooseFunction()
+    if choice == 1:
+        searchCatalog()
+    if choice == 2:
+        editCatalog()
 
 def housekeeping():
     openCatalog = open('catalog.txt', 'r')
@@ -29,5 +31,50 @@ def housekeeping():
             openCatalog.close()
             break
     return catalogList
-    
+
+def chooseFunction():
+    choice = int(input("Enter 1 to search the catalog or 2 to edit it: "))
+    return choice
+
+def searchCatalog():
+    info = ""
+    print("Enter 1 to search by title, ")
+    print("Enter 2 to search by author, ")
+    searchType = int(input("or 3 to search by publication date: "))
+    if searchType == 1:
+        info = titleSearch()
+    if searchType == 2:
+        info = authorSearch()
+    if searchType == 3:
+        info = pubDateSearch()
+    print(info)
+
+def titleSearch():
+    count = 0
+    foundIt = False
+    notFound = "Item not in catalog."
+    searchKey = input("Enter the title of the book: ")
+    while count < len(catalogList):
+        info = catalogList[count]
+        if searchKey in info[0]:
+            foundIt = True
+            return info
+        count += 1
+    if foundIt == False:
+        return notFound
+
+def authorSearch():
+    count = 0
+    foundIt = False
+    notFound = "Item not in catalog."
+    searchKey = input("Enter the author of the book: ")
+    while count < len(catalogList):
+        info = catalogList[count]
+        if searchKey in info[1]:
+            foundIt = True
+            return info
+        count += 1
+    if foundIt == False:
+        return notFound
+
 main()
